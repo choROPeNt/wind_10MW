f4853617a2e6b47e83b6cfe578c1a1eedc8b054d
LucasLeeZX <lucasleezhengxi2014@gmail.com>
2024-09-05 16:44:31 +0200

    Changed abs to relative path

--- scripts/FlapandEdgeDisp.py
+++ scripts/FlapandEdgeDisp.py
@@ -1,36 +1,43 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os as os 
+
+# Current Working Directory
+print("Current working directory = " + os. getcwd())
+print()
 
 # Load the CSV files
-input_Edgewise_Pre = r'C:\Abaqus_temp\scripts\CSV Files\Edgewise-U10-Precone.CSV'
-input_Flapwise_Pre = r'C:\Abaqus_temp\scripts\CSV Files\Flapwise-U10-Precone.CSV'
+# Edgewise and Flapwise results for U = 10
+input_Edgewise_Pre = r'..\wind_10MW\scripts\CSV Files\Displacements\Edgewise-U10-Precone.csv'
+input_Flapwise_Pre = r'..\wind_10MW\scripts\CSV Files\Displacements\Flapwise-U10-Precone.CSV'
 EdgeDisp_Pre = np.loadtxt(input_Edgewise_Pre, delimiter=',')
 FlapDisp_Pre = np.loadtxt(input_Flapwise_Pre, delimiter=',')
 
-input_Edgewise_NPre = r'C:\Abaqus_temp\scripts\CSV Files\Edgewise-U10-NoPrecone.CSV'
-input_Flapwise_NPre = r'C:\Abaqus_temp\scripts\CSV Files\Flapwise-U10-NoPrecone.CSV'
+
+input_Edgewise_NPre = r'..\wind_10MW\scripts\CSV Files\Displacements\Edgewise-U10-NoPrecone.CSV'
+input_Flapwise_NPre = r'..\wind_10MW\scripts\CSV Files\Displacements\Flapwise-U10-NoPrecone.CSV'
 EdgeDisp_NPre = np.loadtxt(input_Edgewise_NPre, delimiter=',')
 FlapDisp_NPre = np.loadtxt(input_Flapwise_NPre, delimiter=',')
 
-input_Horcas    = r'C:\Abaqus_temp\scripts\CSV Files\Horcas et al.CSV'
-input_Kang      = r'C:\Abaqus_temp\scripts\CSV Files\Kang et al.CSV' 
-input_SPWing    = r'C:\Abaqus_temp\scripts\CSV Files\SP Wing.CSV'
+# Results from papers 
+input_Horcas    = r'..\wind_10MW\scripts\CSV Files\Displacements\Horcas et al.CSV'
+input_Kang      = r'..\wind_10MW\scripts\CSV Files\Displacements\Kang et al.CSV' 
+input_SPWing    = r'..\wind_10MW\scripts\CSV Files\Displacements\SP Wing.CSV'
 Horcas  = np.loadtxt(input_Horcas, delimiter=',', skiprows=1)
 Kang    = np.loadtxt(input_Kang, delimiter=',', skiprows=1)
 SPWing  = np.loadtxt(input_SPWing, delimiter=',', skiprows=1)
 
-input_CFDEdge = r'C:\Abaqus_temp\scripts\CSV Files\CFD_Study_Edge.CSV'
+input_CFDEdge = r'..\wind_10MW\scripts\CSV Files\Displacements\CFD_Study_U10 Edge.CSV'
 CFDEdge = np.loadtxt(input_CFDEdge, delimiter=',', skiprows=1)
 
-input_Horcas11Flap = r'C:\Abaqus_temp\scripts\CSV Files\CFD_study_U=11 Flap.CSV'
-input_Horcas11Edge = r'C:\Abaqus_temp\scripts\CSV Files\CFD_study_U=11 Edge.CSV'
-
+input_Horcas11Flap = r'..\wind_10MW\scripts\CSV Files\Displacements\CFD_study_U11 Flap.CSV'
+input_Horcas11Edge = r'..\wind_10MW\scripts\CSV Files\Displacements\CFD_study_U11 Edge.CSV'
 Horcas11Edge = np.loadtxt(input_Horcas11Edge, delimiter=',', skiprows=1)
 Horcas11Flap = np.loadtxt(input_Horcas11Flap, delimiter=',', skiprows=1)
 
-# Z coordinate CSV file
-input_z = r'C:\Abaqus_temp\scripts\CSV Files\coorZ_matrix.CSV'
+# # Z coordinate CSV file
+input_z = r'..\wind_10MW\scripts\CSV Files\coorZ_matrix.CSV'
 CoorZ = np.loadtxt(input_z, delimiter=',')
 
 EdgeDisp_Pre = EdgeDisp_Pre[:,1]
@@ -39,10 +46,10 @@ FlapDisp_Pre = FlapDisp_Pre[:,1]
 EdgeDisp_NPre = EdgeDisp_NPre[:,1]
 FlapDisp_NPre = FlapDisp_NPre[:,1]
 
-print(EdgeDisp_Pre.shape)
-print()
-print(FlapDisp_Pre.shape)
-print(CoorZ.shape)
+# print(EdgeDisp_Pre.shape)
+# print()
+# print(FlapDisp_Pre.shape)
+# print(CoorZ.shape)
 
 # Create separate plots
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
@@ -58,6 +65,7 @@ ax1.set_title('Edgewise Displacement vs. Z Coordinate')
 ax1.set_xlabel('Z Coordinate')
 ax1.set_ylabel('Edgewise Displacement')
 ax1.grid(True)
+
 
 
 # Plot Flapwise Displacement
